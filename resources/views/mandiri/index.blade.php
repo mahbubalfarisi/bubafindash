5eca4273b2c27a3ed09fb5d3f090a175545e24e1
Buba <mahbub.alfarisi@gmail.com>
2019-05-10 10:46:47 +0700

    Make Mandiri Summary Fixed Position

--- resources/views/mandiri/index.blade.php
+++ resources/views/mandiri/index.blade.php
@@ -9,7 +9,7 @@
 	        </div>
         </div>
         <div class="row">
-          	<div class="col-md-12">
+          	<div class="col-md-7">
             	<div class="card">
 				 	@if(session()->get('success'))
 				    	<div class="alert alert-success alert-block">
@@ -44,22 +44,6 @@
 							            <td>{{$mandiri->description}}</td>
 							            <td class="text-right">{{'Rp'.$mandiri->credit}}</td>
 							            <td class="text-right">{{'Rp'.$mandiri->debit}}</td>
-							            <td class="text-right">
-						            		<a href="{{ route('mandiri.edit',$mandiri->id)}}">
-							            		<button class="btn btn-primary">
-							            			<i class="far fa-edit"></i>
-							            		</button>
-						            		</a>
-							            </td>
-							            <td class="text-center">
-							                <form action="{{ route('mandiri.destroy', $mandiri->id)}}" method="post" style="margin-bottom: 0 !important">
-							                  	@csrf
-							                  	@method('DELETE')
-							                  	<button class="btn btn-success" type="submit">
-							                  		<i class="fas fa-check"></i>
-							                  	</button>
-							                </form>
-							            </td>
 							        </tr>
 							        @endforeach
 		                    	</tbody>
@@ -73,7 +57,7 @@
               		</div>
             	</div>
           	</div>
-          	<div class="col-md-12">
+          	<div class="col-md-4" style="position: fixed; top: 11em; right: 1em;">
           		<div class="card">
               		<div class="card-header">
                 		<h4 class="card-title">Mandiri Summary</h4>
@@ -82,26 +66,26 @@
                 		<div class="table-responsive">
                   			<table class="table">
                     			<thead class=" text-primary">
-			                      	<th>
+			                      	<th class="text-center">
 			                        	Total Credit
 			                      	</th>
-			                      	<th>
+			                      	<th class="text-center">
 			                        	Total Debit
 			                      	</th>
-			                      	<th>
+			                      	<th class="text-center">
 			                        	Balance
 			                      	</th>
 		                    	</thead>
 	                    		<tbody>
 									<tr>
-										<td class="text-right">
-											{{$creditmandiri}}
+										<td class="text-center">
+											{{'Rp.'.$creditmandiri}}
 										</td>
-										<td class="text-right">
-											{{$debitmandiri}}
+										<td class="text-center">
+											{{'Rp.'.$debitmandiri}}
 										</td>
-										<td class="text-right">
-											{{$balancemandiri}}
+										<td class="text-center">
+											{{'Rp.'.$balancemandiri}}
 										</td>
 									</tr>
 								</tbody>
