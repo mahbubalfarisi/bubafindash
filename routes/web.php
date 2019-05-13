753fa65de7d022fadc9247ae6a73703f73880952
Buba <mahbub.alfarisi@gmail.com>
2019-05-13 13:44:42 +0700

    Fix Routing from Flexi Server

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/cashes/index', ['uses' => 'CashcalcController@cashview']);
 Route::get('/cryptos/index', ['uses' => 'CryptocalcController@cryptoview']);
 Route::get('/ovo/index', ['uses' => 'OvocalcController@ovoview']);
 Route::get('/jenius/index', ['uses' => 'JeniuscalcController@jeniusview']);
-Route::get('/flexi/index', ['uses' => 'FlexicalcController@jeniusview']);
+Route::get('/flexi/index', ['uses' => 'FlexicalcController@flexiview']);
 
 Route::resource('debts', 'DebtController');
 Route::resource('mandiri', 'MandiriController');
